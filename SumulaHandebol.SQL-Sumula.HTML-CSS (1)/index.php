1719a3b804faca0ab9e0f5e57d53db968b4ceb31
unknown <vitoritali67@gmail.com>
2022-11-23 21:58:44 -0300

    add

--- SumulaHandebol.SQL-Sumula.HTML-CSS (1)/index.php
+++ SumulaHandebol.SQL-Sumula.HTML-CSS (1)/index.php
@@ -3,17 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=for, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <form action="principal.php" method="post">
-    <label for="">Usuario</label>    
-    <input type="text" name="usuario" id="">
-    <label for="">senha</label>
-    <input type="text" name="senha" id="">
-
-    <input type="submit" value="login">
-    </form>
+    
 </body>
 </html>
